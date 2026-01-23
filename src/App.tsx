fd7cf0c82f75f4e5d7ace1fbc6b58bf86864f0e4
Ivan Starikov <vanya.st.ov@gmail.com>
2026-01-23 10:30:40 +0300

    Prepare the first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useRef } from "react";
+
+const siteUrl = "http://localhost:5173/tick-tick-stat/";
+
 function App() {
-  return <>Tick tick stat</>;
+  const formRef = useRef(null);
+
+  const clientId = import.meta.env.VITE_CLIENT_ID;
+  const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+  const handleSubmit = () => {
+    if (formRef.current) {
+      formRef.current.submit();
+    }
+  };
+  return (
+    <>
+      <form
+        ref={formRef}
+        action="https://ticktick.com/oauth/authorize"
+        method="POST"
+        style={{ display: "none" }}
+      >
+        <input type="hidden" name="client_id" value={clientId} />
+        <input type="hidden" name="scope" value="scope" />
+        <input type="hidden" name="state" value="state" />
+        <input type="hidden" name="redirect_uri" value={siteUrl} />
+        <input type="hidden" name="response_type" value="code" />
+      </form>
+
+      <button onClick={handleSubmit}>Redirect via POST with parameters</button>
+    </>
+  );
 }
 
 export default App;
